ebc7712a74a0f4912704c8fc29cc95a24b9d1c20
Dilum Balaarachchi <dilumbal@Dilums-MacBook-Pro.local>
2024-04-27 14:29:03 +1000

    SIT7070 - 4.1P - Decision table task testing code

--- 4.1P/task4_1P/src/test/java/sit707_week4/LoginFormTest.java
+++ 4.1P/task4_1P/src/test/java/sit707_week4/LoginFormTest.java
@@ -12,24 +12,110 @@ public class LoginFormTest
 
 	@Test
 	public void testStudentIdentity() {
-		String studentId = null;
+		String studentId = "s220459698";
 		Assert.assertNotNull("Student ID is null", studentId);
 	}
 
 	@Test
 	public void testStudentName() {
-		String studentName = null;
+		String studentName = "DILUM";
 		Assert.assertNotNull("Student name is null", studentName);
 	}
 	
 	@Test
     public void testFailEmptyUsernameAndEmptyPasswordAndDontCareValCode()
     {
+		//1
 		LoginStatus status = LoginForm.login(null, null);
 		Assert.assertTrue( status.isLoginSuccess() == false );
+		Assert.assertEquals("Empty Username", status.getErrorMsg());
     }
 	
-	/*
-	 * Write more test functions below.
-	 */
+	@Test
+	public void testFailEmptyUsernameAndWrongPasswordAndDontCarValCode()
+    {
+		//2
+		LoginStatus status = LoginForm.login(null, "123");
+		Assert.assertTrue( status.isLoginSuccess() == false );
+		Assert.assertEquals("Empty Username", status.getErrorMsg());
+    }
+	@Test
+	public void testFailEmptyUsernameAndCorrectPasswordAndDontCarValCode()
+    {
+		//3
+		LoginStatus status = LoginForm.login(null, "ahsan_pass");
+		Assert.assertTrue( status.isLoginSuccess() == false );
+		Assert.assertEquals("Empty Username", status.getErrorMsg());
+    }
+	
+	@Test
+	public void testFailWrongUsernameAndEmptyPasswordAndDontCarValCode()
+    {
+		//4
+		LoginStatus status = LoginForm.login("abc", "");
+		Assert.assertTrue( status.isLoginSuccess() == false );
+		Assert.assertEquals("Empty Password", status.getErrorMsg());
+    }
+	
+	@Test
+	public void testFailWrongUsernameAndWrongPasswordAndDontCarValCode()
+    {
+		//5
+		LoginStatus status = LoginForm.login("abc", "123");
+		Assert.assertTrue( status.isLoginSuccess() == false );
+		Assert.assertEquals("Credential mismatch", status.getErrorMsg());
+    }
+	
+	@Test
+	public void testFailWrongUsernameAndCorrectPasswordAndDontCarValCode()
+    {
+		//6
+		LoginStatus status = LoginForm.login("ABC", "ahsan_pass");
+		Assert.assertTrue( status.isLoginSuccess() == false );
+		Assert.assertEquals("Credential mismatch", status.getErrorMsg());
+    }
+	@Test
+	public void testFailCorrectUsernameAndEmptyPasswordAndDontCarValCode()
+    {
+		//7
+		LoginStatus status = LoginForm.login("ahsan", "");
+		Assert.assertTrue( status.isLoginSuccess() == false );
+		Assert.assertEquals("Empty Password", status.getErrorMsg());
+    }
+	@Test
+	public void testFailCorrectUsernameAndWrongPasswordAndDontCarValCode()
+    {
+		//8
+		LoginStatus status = LoginForm.login("ahsan", "123");
+		Assert.assertTrue( status.isLoginSuccess() == false );
+		Assert.assertEquals("Credential mismatch", status.getErrorMsg());
+    }
+	@Test
+	public void testPassCorrectUsernameAndCorrectPasswordAndReturncorrectValCode()
+    {
+		//9
+		LoginStatus status = LoginForm.login("ahsan", "ahsan_pass");
+		Assert.assertTrue( status.isLoginSuccess() == true );
+		//Assert.assertEquals("Credential mismatch", status.getErrorMsg());
+		  Assert.assertTrue(status.getErrorMsg().contains("123456")); // Assuming validation code is "123456"
+    }
+	
+	// testing for validatecode
+	  @Test
+	  public void testValidateCodeEmpty() {
+	    Assert.assertFalse(LoginForm.validateCode(null));
+	  }
+
+	  @Test
+	  public void testValidateCodeWrong() {
+	    Assert.assertFalse(LoginForm.validateCode("abcd"));
+	  }
+
+	  @Test
+	  public void testValidateCodeCorrect() {
+	    LoginStatus status = LoginForm.login("ahsan", "ahsan_pass");
+	    String validationCode = status.getErrorMsg(); // Assuming validation code is retrieved from errorMsg
+	    Assert.assertTrue(LoginForm.validateCode(validationCode));
+	  }
+	
 }
