b40453fab7cb6b41a2584f4168bbde58bea4087a
dhash-deakin <dilumbal@Dilums-MacBook-Pro.local>
2024-05-17 08:02:03 +1000

    9.1P - Login Page Functional Testing Completed

--- 9.1P/springws/src/test/java/web/service/TestMathQuestionService.java
+++ 9.1P/springws/src/test/java/web/service/TestMathQuestionService.java
@@ -61,7 +61,7 @@ public class TestMathQuestionService {
     /*** start of Happy path Testing ***/
     @Test
     public void testLoadHomePage() {
-        System.out.println("Driver info: " + driver);
+    	System.out.println("++++++Test Case 1 - Application Web URL Loads correclty ++++++");
         driver.navigate().to("http://localhost:8080/");
         sleep(5);
         
@@ -86,7 +86,7 @@ public class TestMathQuestionService {
     
     @Test
     public void testLoginSuccessforValidUsernameandValidPassword() {
-        System.out.println("Driver info: " + driver);
+        System.out.println("++++++Test Case 2 - Successfull Login ++++++");
         driver.navigate().to("http://localhost:8080/login");
         sleep(5);
 
@@ -117,7 +117,7 @@ public class TestMathQuestionService {
     
     @Test
     public void testValidInputsValidResultForQ1() {
-        
+    	System.out.println("++++++Test Case 3 - Successful Results of Q1 ++++++");
         // Input
         String firstNumber = "10";
         String secondNumber = "5";
@@ -143,6 +143,7 @@ public class TestMathQuestionService {
     
     @Test
     public void testValidInputsValidResultForQ2() {
+    	System.out.println("++++++Test Case 4 - Successful Results of Q2 ++++++");
         
         // Input
         String firstNumber = "10";
@@ -169,6 +170,7 @@ public class TestMathQuestionService {
     
     @Test
     public void testValidInputsValidResultForQ3() {
+    	System.out.println("++++++Test Case 5 - Successful Results of Q3 ++++++");
     
         
         // Input
@@ -200,4 +202,115 @@ public class TestMathQuestionService {
     
     }
     /*** end of Happy path Testing ***/
+    
+    /*** Login page failure testing***/ 
+    @Test
+    public void testLoginFailureforValidUsernameandInvalidPassword() {
+    	System.out.println("++++++Test Case 6 - Failure - Login - Valid Username - Invalid Password ++++++");
+        driver.navigate().to("http://localhost:8080/login");
+        sleep(5);
+
+     // Find username element
+     		WebElement ele = driver.findElement(By.id("username"));
+     		ele.clear();
+     		ele.sendKeys("dilum");
+     		
+     		// Find password element
+     		ele = driver.findElement(By.id("passwd"));
+     		ele.clear();
+     		ele.sendKeys("wrong_pass");
+     		
+     		// Find DoB element
+     	    WebElement dob = driver.findElement(By.id("dob")); 
+     	    dob.clear();
+     	    dob.sendKeys("11/11/2000");
+     		
+     		// Find Submit button, and click on button.
+     		ele = driver.findElement(By.cssSelector("[type=submit]"));
+     		ele.submit();
+     		
+     		WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebElement messageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Incorrect credentials.')]")));
+            sleep(5);
+            Assert.assertEquals("Incorrect credentials.", messageElement.getText());
+        
+     		sleep(5);
+    }
+    
+    @Test
+    public void testLoginFailureforInValidUsernameandValidPassword() {
+    	System.out.println("++++++Test Case 7 - Failure - Login - InValid Username - Valid Password ++++++");
+        driver.navigate().to("http://localhost:8080/login");
+        sleep(5);
+
+     // Find username element
+     		WebElement ele = driver.findElement(By.id("username"));
+     		ele.clear();
+     		ele.sendKeys("John");
+     		
+     		// Find password element
+     		ele = driver.findElement(By.id("passwd"));
+     		ele.clear();
+     		ele.sendKeys("dilum_pass");
+     		
+     		// Find DoB element
+     	    WebElement dob = driver.findElement(By.id("dob")); 
+     	    dob.clear();
+     	    dob.sendKeys("11/11/2000");
+     		
+     		// Find Submit button, and click on button.
+     		ele = driver.findElement(By.cssSelector("[type=submit]"));
+     		ele.submit();
+     		
+     		WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebElement messageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Incorrect credentials.')]")));
+            sleep(5);
+            Assert.assertEquals("Incorrect credentials.", messageElement.getText());
+        
+     		sleep(5);
+    }
+    
+    @Test
+    public void testLoginFailureforInValidUsernameandInvalidPassword() {
+    	System.out.println("++++++Test Case 8 - Failure - Login - InValid Username - Invalid Password ++++++");
+        driver.navigate().to("http://localhost:8080/login");
+        sleep(5);
+
+     // Find username element
+     		WebElement ele = driver.findElement(By.id("username"));
+     		ele.clear();
+     		ele.sendKeys("John");
+     		
+     		// Find password element
+     		ele = driver.findElement(By.id("passwd"));
+     		ele.clear();
+     		ele.sendKeys("John_pass");
+     		
+     		// Find DoB element
+     	    WebElement dob = driver.findElement(By.id("dob")); 
+     	    dob.clear();
+     	    dob.sendKeys("11/11/2000");
+     		
+     		// Find Submit button, and click on button.
+     		ele = driver.findElement(By.cssSelector("[type=submit]"));
+     		ele.submit();
+     		
+     		WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebElement messageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Incorrect credentials.')]")));
+            sleep(5);
+            Assert.assertEquals("Incorrect credentials.", messageElement.getText());
+        
+     		sleep(5);
+    }
+    
+    /*** Q1 page failure testing***/ 
+    //public void tesValidNumbersandIncorrectResultForQ1{}
+    //public void testInValidNumber12andInValidResultForQ1{}
+    //public void testInValidNumber1ForQ1{}
+    //public void testInValidNumber2ForQ1{}
+    //public void testInValidResultForQ1{}
+    //public void testInValidNumber12tForQ1{}
+    //public void testInValidNumber1andInvalidResulttForQ1{}
+    //public void testInValidNumber2andInvalidResulttForQ1{}
+    
 }
